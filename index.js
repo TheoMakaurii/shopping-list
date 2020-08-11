a0c93d26e465187c05a6e48d5ffdea3adeabe5a2
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-11 09:26:51 -0400

    format changes

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 'use strict';
 
+function checkOriginalItems(){
+
+  $(".shopping-item-toggle").click(function(){
+   
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+
+  });
+}
+$(checkOriginalItems);
+
+function deleteOriginalItems(){
+  $(".shopping-item-delete").click(function(){
+         
+    $(this).parent().parent().remove();
+    console.log($(this).parent());
+  
+  });    
+}
+$(deleteOriginalItems);
 
 function addItem() {
 
 $("#js-shopping-list-form").submit(e => {
 
     e.preventDefault();
-    
 
     let newItem=`<li><span class = "shopping-item"> ${$("#shopping-list-entry").val()} </span>
     <div class="shopping-item-controls">
@@ -17,67 +35,24 @@ $("#js-shopping-list-form").submit(e => {
       <span class="button-label">delete</span>
     </button>
   </div></li>`;
-    
-    console.log(newItem);
-
 
     $(".shopping-list").append(newItem);
     $("#shopping-list-entry").val('');
 
     function deleteButton(){
-
-      $(".shopping-item-delete").click(function(){
-         
+      $(".shopping-item-delete").click(function(){ 
         $(this).parent().parent().remove();
-        console.log($(this).parent())
-        console.log("test");
-        // $(this).newItem.remove();
       });
-      
     }
-    $(deleteButton)
+    $(deleteButton);
 
     function checkButton(){
-
       $(".shopping-item-toggle").click(function(){
-       
-    
-        
-        $(this).closest('li').toggleClass('shopping-item__checked');
-        
-     
-      })
-    
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+      });
     }
     $(checkButton);
   });
-
 }
-
 $(addItem);
 
-function checkOriginalItems(){
-
-  $(".shopping-item-toggle").click(function(){
-   
-
-    
-    $(this).closest('li').toggleClass('shopping-item__checked');
- 
- 
-  });
-
-}
-$(checkOriginalItems);
-
-function deleteOriginalItems(){
-
-  $(".shopping-item-delete").click(function(){
-         
-    $(this).parent().parent().remove();
-    console.log($(this).parent())
-  
-    });
-      
-}
-$(deleteOriginalItems);
